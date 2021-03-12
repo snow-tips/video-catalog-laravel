7a78810786b04dccd98024c658d9a58fa16f193e
Leandro Neves <leandrosoares6@hotmail.com>
2021-03-12 10:38:37 -0300

    feat(genre): adding model, request, controller, seed and endpoint to genre resource

---
+++ app/Http/Controllers/Api/GenreController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\GenreRequest;
+use App\Models\Genre;
+
+class GenreController extends Controller
+{
+    public function index()
+    {
+        return Genre::all();
+    }
+
+    public function store(GenreRequest $request)
+    {
+        $request->validated();
+
+        return Genre::create($request->all());
+    }
+
+    public function show(Genre $genre)
+    {
+        return $genre;
+    }
+
+    public function update(GenreRequest $request, Genre $genre)
+    {
+        $request->validated();
+
+        $genre->update($request->all());
+        return $genre;
+
+    }
+
+    public function destroy(Genre $genre)
+    {
+        $genre->delete();
+        return response()->noContent();
+    }
+}
