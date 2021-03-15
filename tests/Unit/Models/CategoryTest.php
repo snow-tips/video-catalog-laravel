5d6aae9b53f8bd0c011d72e9c1d91208cc2ed31b
Leandro Neves <leandrosoares6@hotmail.com>
2021-03-15 16:47:41 -0300

    test(category): add unit and feature tests for category model

--- tests/Unit/Models/CategoryTest.php
+++ tests/Unit/Models/CategoryTest.php
@@ -1,48 +1,53 @@
 <?php
 
-namespace Tests\Unit;
+namespace Tests\Unit\Models;
 
 use App\Models\Category;
-use App\Models\Genre;
 use Tests\TestCase;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\Traits\Uuid;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class CategoryTest extends TestCase
 {
-    use DatabaseMigrations;
+    private $category;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->category = new Category();
+    }
+
     public function testFillableAttribute()
     {
-        Genre::create(['name' => 'test']);
         $fillable = ['name', 'description', 'is_active'];
-        $category = new Category();
-        $this->assertEquals($fillable, $category->getFillable());
+        $this->assertEquals($fillable, $this->category->getFillable());
     }
 
     public function testCastsAttribute()
     {
-        $casts = ['id' => 'string'];
-        $category = new Category();
-        $this->assertEquals($casts, $category->getCasts());
+        $casts = [
+            'id' => 'string',
+            'is_active' => 'boolean'
+        ];
+        $this->assertEquals($casts, $this->category->getCasts());
     }
 
     public function testDatesAttibute()
     {
         $dates = ['deleted_at', 'created_at', 'updated_at'];
-        $category = new Category();
 
-        foreach($dates as $date) {
-            $this->assertContains($date, $category->getDates());
-        }
+        foreach($dates as $date) {;
+            $this->assertContains($date, $this->category->getDates());
+        };
 
-        $this->assertCount(count($dates), $category->getDates());
+        $this->assertEqualsCanonicalizing($dates, $this->category->getDates());
+
+        $this->assertCount(count($dates), $this->category->getDates());
     }
 
     public function testIfIncrementAttributeIsDisabled()
     {
-        $category = new Category();
-        $this->assertFalse($category->incrementing);
+        $this->assertFalse($this->category->incrementing);
     }
 
     public function testIfUseTraits()
